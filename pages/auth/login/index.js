e80635e6cb10d5ae0f3e45fe06795a59a48b949f
rahimkhan01 <k.rahim@iitg.ac.in>
2023-09-04 15:41:54 +0530

    some fixes

--- pages/auth/login/index.js
+++ pages/auth/login/index.js
@@ -3,10 +3,7 @@ import SignIn from "@/pages/components/Authorization/SignIn";
 export default function Login() {
     return (
         <div>
-            <Navbar></Navbar>
-            <div>
                 <SignIn></SignIn>
-            </div>
         </div>
     )
 };
