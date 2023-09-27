4dc8f8ba47a7695d3b635ed047fb4308093dd8dd
Rahim Khan <109789890+rahim-khan-iitg@users.noreply.github.com>
2023-09-28 03:01:35 +0530

    problem page completed

--- pages/api/DB/FetchProblems.js
+++ pages/api/DB/FetchProblems.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-
+import connection from "@/database/conn";
 export default async function handler(req, res) {
     try
     {
-        const problems=await axios.get("https://64fc6e0b605a026163ae7bdc.mockapi.io/problems");
-        return res.status(200).json(problems.data);
+        // const problems=await axios.get("https://64fc6e0b605a026163ae7bdc.mockapi.io/problems");
+        const conn = await connection();
+        const [rows, fields] = await conn.execute("SELECT * FROM problems");
+        // console.log(rows);
+        return res.status(200).json(rows);
     }
     catch(err)
     {
--- pages/components/Home/ProblemPage.js
+++ pages/components/Home/ProblemPage.js
@@ -1,32 +1,42 @@
-import React from 'react'
-import Editor from '../Editor/Codeeditor'
-// pages/problems/[id].js
+import { useEffect,useState } from 'react';
+import Editor from '../Editor/Codeeditor';
 import { useRouter } from 'next/router';
-
-// const ProblemPage = () => {
-
-//   return (
-    
-//   );
-// };
-
-// export default ProblemPage;
-import { useState } from 'react';
+import axios from 'axios';
 
 const ProblemPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('/api/DB/FetchSingleProblem', { id });
+        console.log(response.data);  // Assuming the problem data is logged
+        setDescription(response.data[0].description);
+        setId1(response.data[0].id);
+        setTitle(response.data[0].title);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+  const [id1,setId1] = useState();
+  const [title,setTitle]=useState();
+  const [description,setDescription]=useState();
   return (
     <div className='grid grid-cols-3'>
       <div className='p-3'>
-        problem goes here
+      <h1 className='font-bold'>{title}</h1>
+      <p>{description}</p>
       </div>
       <div className='col-span-2'>
-        <Editor/>
+        <Editor />
       </div>
     </div>
-    
   );
 };
 
